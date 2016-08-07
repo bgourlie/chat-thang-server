285296a774d51966c58dc17839d0c723a20b6d1d
W. Brian Gourlie <bgourlie@gmail.com>
2016-08-07 13:59:22 -0500

    CLI options

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ extern crate log;
 extern crate env_logger;
 extern crate ws;
 extern crate serde_json;
+extern crate clap;
 
 use ws::{listen, Sender};
+use clap::{Arg, App};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Message {
@@ -18,10 +20,32 @@ struct Message {
 }
 
 fn main() {
+    let matches = App::new("chat-thang-server")
+        .version("0.1")
+        .author("W. Brian Gourlie <bgourlie@gmail.com>")
+        .about("A stupid chat thing")
+        .arg(Arg::with_name("bind_ip")
+            .short("i")
+            .long("ip")
+            .help("IP address to bind the server to")
+            .takes_value(true))
+        .arg(Arg::with_name("bind_port")
+            .short("p")
+            .long("port")
+            .help("The port to bind the server to")
+            .takes_value(true))
+        .get_matches();
+
     env_logger::init().unwrap();
 
+    let bind_addr = {
+        let bind_ip = matches.value_of("bind_ip").unwrap_or("localhost");
+        let bind_port = matches.value_of("bind_port").unwrap_or("2794");
+        format!("{}:{}", bind_ip, bind_port)
+    };
+
     // Listen on an address and call the closure for each connection
-    if let Err(error) = listen("127.0.0.1:2794", |out: Sender| {
+    if let Err(error) = listen(&*bind_addr, |out: Sender| {
 
         // The handler needs to take ownership of out, so we use move
         move |msg: ws::Message| {
@@ -29,7 +53,9 @@ fn main() {
             match msg {
                 ws::Message::Text(json) => {
                     match serde_json::from_str::<Message>(&json) {
-                        Ok(deserialized) => out.broadcast(serde_json::to_string(&deserialized).unwrap()),
+                        Ok(deserialized) => {
+                            out.broadcast(serde_json::to_string(&deserialized).unwrap())
+                        }
                         Err(err) => {
                             let err_msg = format!("Deserialization failed: {:?}", err);
                             error!("{}", err_msg);
